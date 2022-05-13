4f2653bf6fc9aa681cd29be5832c860251eac7cf
Vadim Lazev <Vadim.Lazev@bia-tech.ru>
2022-05-13 12:56:08 +0300

    Add methods SAMLResponse and getted user

--- app.php
+++ app.php
@@ -5,8 +5,10 @@ use \GuzzleHttp\Client;
 use \GuzzleHttp\Psr7\Request;
 use \GuzzleHttp\Cookie\CookieJar as CookieHandler;
 use \GuzzleHttp\Command\Guzzle\GuzzleClient;
+use Exception;
 
 
+ini_set('display_errors', 0);
 
 
 class AdapterIQQ {
@@ -20,25 +22,30 @@ class AdapterIQQ {
 
 
     private $urls_path = [
+        'auth' => '',
         'filter' => '/path/to/action',
         'get' => '/path/to/action',
         'filter_group' => '/path/to/action',
     ];
 
-    private $state = [];
+    private $data = [];
 
-    private $flow_token;
+    private $SAMLResponse;
     private $uaid;
  
-//    private $client_id = 'e37ffdec11c0245cb2e0';
- 
-
     public function __construct()
     {
         $this->client = new Client(['cookies' => true]);
+       # $this->client->setDefaultOptions('verify', false);
+        $this->init_process_login();
+    }
+
+    private function init_process_login()
+    {
         $this->get_landing_context_azure();
-        // $this->send_username_info();
-        // $this->do_auth_in_azure();
+        $this->send_username_info();
+        $this->do_auth_in_azure();
+        $this->get_session_iiq_service();
     }
 
     public function get_landing_context_azure()
@@ -50,12 +57,12 @@ class AdapterIQQ {
             'User-Agent'=> 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36'
          ];
      
-        $landing = $client->request('GET','https://office.com/login', [/*'headers' => $headers_init,*/ 
-                                                                        'debug'=> true,
+        $landing = $client->request('GET', $this->url_base, [/*'headers' => $headers_init,*/ 
+                                                                        'debug'=> false,
+                                                                        'verify' => false,
                                                                         'allow_redirects' => ['track_redirects' => true]]);
             $raw_html = $landing->getBody()->getContents();
             $cookie_jar = $client->getConfig('cookies');
-            $cookie = $cookie_jar->toArray();
             $headers = $landing->getHeaders();
             $historyUri = $landing->getHeader(\GuzzleHttp\RedirectMiddleware::HISTORY_HEADER);
             
@@ -63,20 +70,21 @@ class AdapterIQQ {
                 'flowToken' => $this->search_in_serverData('sFT', $raw_html),
                 'ctx' => $this->search_in_serverData('sCtx', $raw_html),
                 'requestId' => $this->search_in_serverData('requestId', $raw_html),
-                'apiCanary' => $this->search_in_serverData('apiCanaty', $raw_html),
+                'apiCanary' => $this->search_in_serverData('apiCanary', $raw_html),
                 'canary' => $this->search_in_serverData('canary', $raw_html),
                 'correlationId' => $this->search_in_serverData('correlationId', $raw_html),
-                'x-ms-request-id' => $headers['x-ms-request-id'][0]
+                'x-ms-request-id' => $headers['x-ms-request-id'][0],
+                'client-request-id' => $this->search_in_serverData('correlationId', $raw_html),
+                'hpgrequestid' => $this->search_in_serverData('sessionId', $raw_html),
+                'Referer' => $historyUri[1]
             ];
-           // var_dump($data);
-            var_dump($this->parse_uri($historyUri[1]));
+
+            $this->data = $data;
+            // var_dump($data);
+
         }
         
-        
-    private function parse_uri($link_microsoftonline){
-        return parse_url($link_microsoftonline);
-    }   
-    
+
 
     private function search_in_serverData($name, $data)
     {
@@ -92,37 +100,43 @@ class AdapterIQQ {
         $client = $this->client;
 
         $headers_for_login = [
-            'Host' => 'login.live.com',
             'Accept' => 'application/json',
-            'hpgact' => '0',
+            'Referer' => $this->data['Referer'],
+            'Origin' => 'https://login.microsoftonline.com',
+            'client-request-id' => $this->data['client-request-id'],
+            'hpgact' => '1900',
+            'hpgid' => '1104',
+            'Host' => 'login.microsoftonline.com',
+            'hpgrequestid' => $this->data['hpgrequestid'],
             'Content-Type' => 'application/json; charset=UTF-8',
-            'client-request-id' => $this->uaid,
             'User-Agent' => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36'    
         ];
 
         
         $body_request = [
             'checkPhones' => false,
-            'flowToken' => $this->flow_token,
+            'country' => 'DE',
+            'federationFlags' => 0,
+            'flowToken' => $this->data['flowToken'],
+            'forceotclogin' => false,
+            'isAccessPassSupported' => true,
             'isCookieBannerShown' => false,
+            'isExternalFederationDisallowed' => false,
             'isFidoSupported' => false,
-            'isOtherIdPSupported' => false,
-            'uaia' => $this->uaid,
+            'isOtherIdpSupported' => true,
+            'isRemoteConnectSupported' => false,
+            'isRemoteNGCSupported' => true,
+            'isSignup' => false,
+            'originalRequest' => $this->data['ctx'],
             'username' => $this->login_info['username'],
         ];
 
         $cookie_jar = $client->getConfig('cookies');
 
-        $send_login = $client->request('POST', 'https://login.live.com/GetCredentialType.srf',
+        $send_login = $client->request('POST', 'https://login.microsoftonline.com/common/GetCredentialType?mkt=en-US',
                                         ['headers' => $headers_for_login,
                                          'json' => $body_request,
                                          'cookies' => $cookie_jar] );
-
-     //   $response_login = $send_login->getBody()->getContents();
-    //     var_dump($this->flow_token);
-    //     var_dump($client->getConfig('cookies'));
-        
-    //    var_dump($response_login);
 
     }
 
@@ -131,11 +145,15 @@ class AdapterIQQ {
         $client = $this->client;
 
         $headers_for_auth = [
-            'Host' => 'login.live.com',
-            'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'hpgact' => '0',
-            'Content-Type' => 'application/x-www-form-urlencoded',
-            'client-request-id' => $this->uaid,
+            'Accept' => 'application/json',
+            'Referer' => $this->data['Referer'],
+            'Origin' => 'https://login.microsoftonline.com',
+            'client-request-id' => $this->data['client-request-id'],
+            'hpgact' => '1900',
+            'hpgid' => '1104',
+            'Host' => 'login.microsoftonline.com',
+            'hpgrequestid' => $this->data['hpgrequestid'],
+            'Content-Type' => 'application/json; charset=UTF-8',
             'User-Agent' => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36'    
         ];
 
@@ -150,14 +168,14 @@ class AdapterIQQ {
             'hisRegion' => '',
             'hisScaleUnit' => '',
             'passwd' => $this->login_info['passwd'],
-            'ps' => '',
+            'ps' => '2',
             'psRNGCDefaultType'=> '',
             'psRNGCEntropy' => '',
             'psRNGCSLK' => '',
-            'canary' => '',
-            'ctx' => '',
-            'hpgrequestid' => '',
-            'PPFT' => $this->flow_token,
+            'canary' => $this->data['canary'],
+            'ctx' => $this->data['ctx'],
+            'hpgrequestid' => $this->data['hpgrequestid'],
+            'flowToken' => $this->data['flowToken'],
             'PPSX' => 'P',
             'NewUser' => '1',
             'FoundMSAs' => '',
@@ -166,26 +184,112 @@ class AdapterIQQ {
             'CookieDisclosure' => '0',
             'IsFidoSupported' => '1',
             'isSignupPost' => '0',
-            'i19' => '3380'
+            'i19' => '35114'
         ];
+
         $prepare_cookie = $client->getConfig('cookies');
-        $request_auth = $client->request('POST', 'https://login.live.com/ppsecure/post.srf', 
+        $request_auth = $client->request('POST', $this->get_login_uri($this->data['Referer']), 
             ['headers' => $headers_for_auth,
              'form_params'=> $json , 
              'cookies' => $prepare_cookie, 
              'debug' => true]);
 
+        $historyUri = $request_auth->getHeader(\GuzzleHttp\RedirectMiddleware::HISTORY_HEADER);
+
         $response_auth = $request_auth->getBody()->getContents();
+        $this->parse_SALMResponse($response_auth);
+        #$cookie_jar = $client->getConfig('cookies');
+        
+
+    }
+
+    public function parse_SALMResponse($raw_html)
+    {
+        $regExpFlowToken = '#name="SAMLResponse" value="("|)([^"]+)"#';
+        preg_match($regExpFlowToken, $raw_html, $saml_matches);
+        $this->SAMLResponse = $saml_matches[2];
+    }
+
+    private function get_login_uri($referer)
+    {
+        $parse_uri_referer = parse_url($referer);
+        $parse = explode('/', $parse_uri_referer['path']);
+        return 'https://login.microsoftonline.com/'.$parse[1].'/login';
+    }
+
+
+    private function get_session_iiq_service()
+    {
+        $client = $this->client;
+        //TODO поправить заголовки по феншую. 
+        $headers_for_session_iiq = [
+            'Accept' => 'application/json',
+            'Referer' => $this->data['Referer'],
+            'Origin' => 'https://login.microsoftonline.com',
+            'client-request-id' => $this->data['client-request-id'],
+            'hpgact' => '1900',
+            'hpgid' => '1104',
+            'Host' => 'login.microsoftonline.com',
+            'hpgrequestid' => $this->data['hpgrequestid'],
+            'Content-Type' => 'application/json; charset=UTF-8',
+            'User-Agent' => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36'    
+        ];
         $cookie_jar = $client->getConfig('cookies');
-        var_dump($cookie_jar);
+        $json = [
+            'SAMLResponse' => $this->SAMLResponse,
+            '' => $this->url_base.$this->urls_path['auth']
+        ];
+
+
+        $init_session = $client->request('POST', $this->url_base, ['headers' => $headers_for_session_iiq, 
+                                                                    'debug'=> false,
+                                                                    'verify' => false,
+                                                                    'cookie' => $cookie_jar,
+                                                                    'allow_redirects' => ['track_redirects' => true]]);
+        
+        
 
     }
 
 
-    // public function do_request_to_iqq_service($path, $data)
-    // {
-      
-    // }
+
+    public function get_user($user_id)
+    {
+        $client = $this->client;
+        //TODO поправить заголовки по феншую. 
+        $headers_for_session_iiq = [
+            'Accept' => 'application/json',
+            'Referer' => $this->data['Referer'],
+            'Origin' => 'https://login.microsoftonline.com',
+            'client-request-id' => $this->data['client-request-id'],
+            'hpgact' => '1900',
+            'hpgid' => '1104',
+            'Host' => 'login.microsoftonline.com',
+            'hpgrequestid' => $this->data['hpgrequestid'],
+            'Content-Type' => 'application/json; charset=UTF-8',
+            'User-Agent' => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36'    
+        ];
+
+        $cookie_jar = $client->getConfig('cookies');
+
+        $json = [
+            'SAMLResponse' => $this->SAMLResponse,
+            '' => $this->url_base.$this->urls_path['auth']
+        ];
+
+
+        $request = $client->request('POST', $this->url_base, ['headers' => $headers_for_session_iiq, 
+                                                                    'debug'=> false,
+                                                                    'verify' => false,
+                                                                    'cookie' => $cookie_jar,
+                                                                    'allow_redirects' => ['track_redirects' => true]]);
+        $result = $request->getBody()->getContents();
+
+        return $result;
+    }
+
+
+
 
 }
 
